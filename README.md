c6c20d0c8613ba2ea48abce2fc1dc87a780d6300
wuxiaoyang <15290491277@163.com>
2018-04-07 19:59:52 +0800

    完善 dist打包

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # todolist-vue应用
+
+# wuxy720
+
+# 描述
+这是一个关于基于webpack+vue的简单的todolist应用，实现了一些简单的小功能
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV = "development"
 
@@ -29,20 +30,7 @@ const config = {
 				test: /\.jsx$/,
 				loader: 'babel-loader',
 			},
-			{
-				test: /\.styl$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true,
-						}
-					},
-					'stylus-loader',
-				]
-			},
+			
 			{
 				test: /\.(gif|jpg|jpeg|png|svg)$/,
 				use: [
@@ -68,6 +56,20 @@ const config = {
 }
 
 if(isDev) {
+	config.module.rules.push({
+		test: /\.styl$/,
+		use: [
+		    'style-loader',
+			'css-loader',
+			{
+				loader: 'postcss-loader',
+				options: {
+					sourceMap: true,
+				}
+			},
+			'stylus-loader',
+		]
+	})
 	config.devtool = '#cheap-module-eval-source-map' // 启动编译调试
 	config.devServer = {
 		port: 8000,
@@ -80,6 +82,40 @@ if(isDev) {
 	config.plugins.push(
 		new webpack.HotModuleReplacementPlugin(), // 热更新插件，局部刷新不需要重新载入
 		new webpack.NoEmitOnErrorsPlugin()
+	)
+} else{
+	config.entry = {
+		app: path.join(__dirname, 'src/index.js'),
+		vendor: ['vue', 'vue-router']
+	}
+	config.output.filename = '[name].[chunkhash:8].js'
+	config.module.rules.push({
+		test: /\.styl$/,
+		use: ExtractPlugin.extract({
+			fallback: 'style-loader',
+			use: [
+				'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+						sourceMap: true,
+					}
+				},
+				'stylus-loader',
+			]
+		})
+	})
+	config.plugins.push(
+		// css分离打包
+		new ExtractPlugin('styles.[contentHash:8].css'),
+		// 类库与业务代码分离打包插件
+		new webpack.optimize.CommonChunkPlugin({
+			name: 'vendor'
+		}),
+		// webpack文件单独打包
+		new webpack.optimize.CommonChunkPlugin({
+			name: 'runtime'
+		})
 	)
 }
 
